df7f17bf2dbcd8ca2d2149e383a3f256f68073ed
Argel Cavallini <argel_095@hotmail.com>
2021-09-02 21:06:29 -0300

    commit 01

---
+++ src/index.js
@@ -0,0 +1,141 @@
+const { request, response } = require('express');
+const express = require('express');
+const {v4:uuid} = require('uuid');
+const app = express();
+
+const dados = [];
+
+app.use(express.json());
+
+app.post("/users",(request,response)=>{
+  //Pegar dados do body
+  const { name , username }  = request.body;
+
+  //Verificar se username já existe
+  const usesrnameAlreadyExists = dados.some(
+    (dados) => dados.username === username
+  );
+
+  //Se existir (true)
+  if(usesrnameAlreadyExists){
+    //Retornar aviso informando que já existe
+    response.status(400).json({error: "Username already exists!"});
+  }
+
+  //Se não existir, realiza o push para o array
+  dados.push({
+    id:uuid(),
+    name,
+    username,
+    todos: []
+  })
+
+  //Retorna
+  return response.status(201).send();
+});
+
+app.get("/todos",(request,response)=>{
+  const { username } = request.headers;
+
+  const dado = dados.find(dado => dado.username === username);
+
+  if(!dado){
+    return response.status(400).json({error:"Todos not found!"})
+  }
+
+   return response.json(dado.todos);
+});
+
+app.post("/todos",(request,response)=>{
+  const { username } = request.headers;
+  const { title , deadline }  = request.body;
+
+  const dado = dados.find(dado => dado.username === username);
+
+  if(!dado){
+    return response.status(400).json({error:"Todos not found!"})
+  }
+
+  dado.todos.push({
+    id:uuid(),
+    title,
+    done:false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  })
+
+  return response.status(201).send();
+
+});
+
+app.put("todos/:id", (request,response)=>{
+  const { username } = request.headers;
+  const { id } = request.params;
+  const { title , deadline }  = request.body;
+
+  const dado = dados.find(dado => dado.username === username);
+
+  if(!dado){
+    return response.status(400).json({error:"Todos not found!"})
+  }
+
+  const edit = dado.todos(find(edit => edit.id === id));
+  if(!edit){
+    return response.status(400).json({error:"Todos edit not found!"})
+  }
+
+  if(title != ''){
+    edit.title = title;
+  }
+  if(deadline != ''){
+    edit.deadline = deadline;
+  }
+
+  return response.status(201).send();
+});
+
+app.patch("todos/:id/done", (request,response)=>{
+    const { username } = request.headers;
+    const { id } = request.params;
+
+    const dado = dados.find(dado => dado.username === username);
+
+    if(!dado){
+      return response.status(400).json({error:"Todos not found!"})
+    }
+
+    const edit = dado.todos(find(edit => edit.id === id));
+    if(!edit){
+      return response.status(400).json({error:"Todos edit not found!"})
+    }
+
+    edit.done = true;
+  
+    return response.status(201).send(todo);
+
+});
+
+app.delete("todos/:id",(request,response)=>{
+ const { username } = request.headers;
+    const { id } = request.params;
+
+    const dado = dados.find(dado => dado.username === username);
+
+    if(!dado){
+      return response.status(400).json({error:"Todos not found!"})
+    }
+
+    const edit = dado.todos(find(edit => edit.id === id));
+    if(!edit){
+      return response.status(400).json({error:"Todos edit not found!"})
+    }
+  
+  //splice
+  edit.splice(edit,1); //ONDE DELETER,QTDE DELETAR
+
+  return response.status(200).json(todos);
+});
+
+app.listen(3333, () => {
+  console.log('Deu boa!');
+});
