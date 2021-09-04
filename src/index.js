e5e7870dc24dda94a6b0163e26c5270fa8e12446
Argel Cavallini <argel_095@hotmail.com>
2021-09-03 21:25:14 -0300

    commit 01

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-const { request, response } = require('express');
 const express = require('express');
+const cors = require('cors');
+
 const {v4:uuid} = require('uuid');
+
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 const dados = [];
 
